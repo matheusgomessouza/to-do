8ca3670925b8c0fa0369c48047456262386b2fee
matheusgomessouza <matheusg_souza@outlook.com>
2021-12-29 17:46:55 -0300

    Challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== '') {
+      setTasks(oldState => [...oldState, {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }]);
+    } else {
+      return;
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toggleTasks = tasks.map(toggleTask => toggleTask.id === id ? {
+      ...toggleTask,
+      isComplete: !toggleTask.isComplete
+    } : toggleTask)
+    setTasks(toggleTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(tasks => tasks.id !== id);
+    setTasks(removeTask);
   }
 
   return (
